500a78858ab75b032d73be6c3003f2f62e21be79
seguinard <anthony.seguinard@live.fr>
2019-09-30 14:26:53 +0200

    Chagement de URL
    id + slug, 
    securiter niveau de l'url, le slug doit correspondre a celui de l'id

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -4,9 +4,11 @@ namespace App\Controller;
 
 use App\Entity\Article;
 use App\Repository\ArticleRepository;
+use Exception;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 
 /**
  * @Route("/article")
@@ -23,10 +25,15 @@ class ArticleController extends AbstractController
     }
 
     /**
-     * @Route("/{slug}", name="article_show", methods={"GET"})
+     * @Route("/{id}-{slug}", name="article_show", methods={"GET"}, requirements={"slug": "[a-z0-9\-]*"})
      */
-    public function show(Article $article): Response
-    {
+    public function show(Request $request, Article $article): Response
+    {   
+        $verif = $request->attributes->get('_route_params');
+
+        if ($verif['slug'] !== $article->getSlug()) {
+            throw new Exception("l'id est le slug ne correspond, ou la page existe pas");
+        }
         return $this->render('article/show.html.twig', [
             'article' => $article,
         ]);
