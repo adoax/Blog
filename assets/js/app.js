e8b87a95d387cd8257b781c23e4762283ca87ce3
seguinard <anthony.seguinard@live.fr>
2019-10-10 14:04:14 +0200

    Revert "React Router"
    This reverts commit ea04fbde723ef947b7d56a38c0733273114039f2.

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,90 @@
-import React from 'react';
+import React from "react";
 import ReactDOM from 'react-dom'
-import Index from './blog/index'
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      error: null,
+      isLoaded: false,
+      items: []
+    };
+  }
+
+  componentDidMount() {
+    fetch("https://127.0.0.1:8000/api/articles")
+      .then(res => res.json())
+      .then(
+        result => {
+          this.setState({
+            isLoaded: true,
+            items: result["hydra:member"]
+          });
+        },
+        // Remarque : il est important de traiter les erreurs ici
+        // au lieu d'utiliser un bloc catch(), pour ne pas passer à la trappe
+        // des exceptions provenant de réels bugs du composant.
+        error => {
+          this.setState({
+            isLoaded: true,
+            error
+          });
+        }
+      );
+  }
+
+  render() {
+    const { error, isLoaded, items } = this.state;
+    if (error) {
+      return <div>Erreur : {error.message}</div>;
+    } else if (!isLoaded) {
+      return <div>Chargement…</div>;
+    } else {
+      return (
+        <div className="row">
+          {items.map(item => (
+            <div className="col-3">
+              <div className="card text-white bg-primary mb-3">
+                <div className="cardheader">
+                  {console.log(item.images[0])}
+                  {item.images[0] ? (
+                    <div>
+                      {item.images.slice(0, 1).map(image => (
+                        <img
+                          src={"./images/articles/" + image.url}
+                          className="img-fluid"
+                        ></img>
+                      ))}
+                    </div>
+                  ) : (
+                    <div> <img src="https://via.placeholder.com/360x230" class="img-fluid"></img> </div>
+                  )}
+                </div>
+                <div className="card-body">
+                  Ville:
+                  {item.options
+                    .map(option => <span> {option.name}</span>)
+                    .reduce(
+                      (acc, x) =>
+                        acc === null ? (
+                          x
+                        ) : (
+                          <>
+                            {acc}, {x}
+                          </>
+                        ),
+                      null
+                    )}
+                  <p className="card-text">{item.extraitContent}</p>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+      );
+    }
+  }
+}
 
 
-ReactDOM.render(<Index />, document.getElementById('root'));
+ ReactDOM.render(<App />, document.getElementById('root'));
